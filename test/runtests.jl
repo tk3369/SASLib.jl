56b35729243728774ef6a3ccdd807580636c3ef3
Tom Kwong <tk3369@gmail.com>
2017-12-09 15:29:50 -0800

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,26 +3,15 @@ using Base.Test
 
 @testset "SASLib" begin
 
-    @testset "getdefault" begin
-        getdefault = SASLib.getdefault
-        dict = Dict(:abc => 1, "b" => 2, 100 => "good")
-        @test getdefault(dict, :abc, "hello") == 1
-        @test getdefault(dict, "b", "hello")  == 2
-        @test getdefault(dict, 100, "hello")  == "good"
-        @test getdefault(dict, :x, "hello") == "hello"
-        @test getdefault(dict, "y", "hello") == "hello"
-        @test getdefault(dict, 200, "hello") == "hello"
-    end
-
-    @testset "openclose" begin
+    @testset "open and close" begin
         handler = SASLib.openfile(SASLib.ReaderConfig("test1.sas7bdat"))
         @test typeof(handler) == SASLib.Handler
         @test SASLib.closefile(handler) == nothing
     end
 
-    @testset "header" begin
-        @test readsas("test1.sas7bdat") == 1
+    @testset "read data" begin
+        df = readsas("test1.sas7bdat")
+        @test size(df) == (10, 100)
     end
-    #nothing
 
 end
