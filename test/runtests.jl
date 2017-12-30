71c1ad1c68757fcff2a93e8d712c8e60a00b656f
Tom Kwong <tk3369@gmail.com>
2017-12-29 18:12:16 -0800

    added unit tests for include/exclude column feature

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,7 +19,7 @@ using Base.Test
         end
     end
 
-    @testset "manual" begin
+    @testset "incremental read" begin
         fname = "test1.sas7bdat"  # 10 rows
         handler = SASLib.open(fname)
         @test handler.config.filename == fname
@@ -31,7 +31,7 @@ using Base.Test
         @test result[:nrows] == 3
     end
 
-    @testset "numeric" begin
+    @testset "various data types" begin
         result = readsas("test1.sas7bdat")
         df = result[:data]
         @test sum(df[:Column1][1:5]) == 2.066
@@ -40,7 +40,7 @@ using Base.Test
         @test df[:Column4][1:3] == [Date("1965-12-10"), Date("1977-03-07"), Date("1983-08-15")]
     end
 
-    @testset "datetime" begin
+    @testset "datetime with missing values" begin
         result = readsas("datetime.sas7bdat")
         df = result[:data]
         @test (result[:nrows], result[:ncols]) == (5, 4)
@@ -48,6 +48,28 @@ using Base.Test
         @test result[:data][:dt][5] == DateTime(2018, 3, 31, 14, 20, 33)
         @test count(ismissing, result[:data][:mtg]) == 1
         @test count(ismissing, result[:data][:dt]) == 3
+    end
+
+    @testset "include/exclude columns" begin
+        result = readsas("productsales.sas7bdat", include_columns=[:MONTH, :YEAR])
+        @test result[:ncols] == 2
+        @test sort(result[:column_symbols]) == sort([:MONTH, :YEAR])
+        
+        result = readsas("productsales.sas7bdat", include_columns=[1, 2, 7])
+        @test result[:ncols] == 3
+        @test sort(result[:column_symbols]) == sort([:ACTUAL, :PREDICT, :PRODUCT])
+
+        result = readsas("productsales.sas7bdat", exclude_columns=[:DIVISION])
+        @test result[:ncols] == 9
+        @test !(:DIVISION in result[:column_symbols])
+
+        result = readsas("productsales.sas7bdat", exclude_columns=collect(2:10))
+        @test result[:ncols] == 1
+        @test sort(result[:column_symbols]) == sort([:ACTUAL])
+
+        # error handling
+        @test_throws SASLib.ConfigError readsas("productsales.sas7bdat", 
+            include_columns=[1], exclude_columns=[1])
     end
 
     @testset "misc" begin
